f9b67d7ea5a8cb7f590e379497a021e96bc9a2ac
Tupran <ole.andreas.gloersen@tupran.no>
2024-11-05 14:05:23 +0100

    Add files via upload

---
+++ testfil4.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Omregning fra C til F
+# ## OAG
+# ### 2024.10.29
+# <h4>Overskrift på nivå 4</h4>
+
+# In[26]:
+
+
+C = 100 # Grader C
+F = C*1.8 + 32 # Grader F
+
+
+# In[28]:
+
+
+print("F =", F)
+
+
+# ![image.png](attachment:8c93df6d-b8ef-4e35-8b4a-f0909f1bd4d4.png)
+
+# In[ ]:
+
+
+
+
